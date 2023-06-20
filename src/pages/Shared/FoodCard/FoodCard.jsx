8138889ea6dcc881b1b30e8e1a56ba0c1e96c453
Imran Hossain <imranhossain0461@gmail.com>
2023-06-20 18:57:07 +0600

    Data delete method added

--- src/pages/Shared/FoodCard/FoodCard.jsx
+++ src/pages/Shared/FoodCard/FoodCard.jsx
@@ -1,9 +1,42 @@
 import React from 'react';
 import { FaEye , FaEdit , FaTrash } from 'react-icons/fa';
+import Swal from 'sweetalert2';
 
 const FoodCard = ({food}) => {
     console.log(food);
     const {_id, category,details ,foodPhoto ,name ,price ,quantity , supplier} = food;
+
+    const handleItemDelete = (_id) => {
+        console.log(_id)
+        Swal.fire({
+            title: 'Are you sure?',
+            text: "You won't be able to revert this!",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Yes, delete it!'
+        }).then((result) => {
+        if (result.isConfirmed) {
+
+
+            fetch(`http://localhost:5000/food/${_id}` , {
+                method: 'DELETE'
+            })
+            .then(res=> res.json())
+            .then(data => {
+                console.log(data);
+                if(data.deletedCount > 0){
+                    Swal.fire(
+                    'Deleted!',
+                    'Food Item has been deleted.',
+                    'success'
+                    )
+                }
+            })  
+        }
+        })
+    }
     
     return (
         <div className='col-lg-6'>
@@ -22,9 +55,9 @@ const FoodCard = ({food}) => {
                </div>
 
                <div className="card-button-group">
-                    <button className='bg-[#D2B48C;]'><FaEye /></button>
+                    <button  className='bg-[#D2B48C;]'><FaEye /></button>
                     <button className='bg-[#3C393B;]'><FaEdit /></button>
-                    <button className='bg-[#EA4744;]'><FaTrash /></button>
+                    <button onClick={()=> handleItemDelete(_id)} className='bg-[#EA4744;]'><FaTrash /></button>
                </div>
             </div>
         </div>
